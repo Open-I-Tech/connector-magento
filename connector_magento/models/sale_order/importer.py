f4785367000f9c4614065bf2ffa0bdf6c3219b13
Romain Deheele <romaindeheele@gmail.com>
2026-05-25 11:47:17 +0200

    [FIX] recompute taxes on imported sale order lines

--- connector_magento/models/sale_order/importer.py
+++ connector_magento/models/sale_order/importer.py
@@ -514,7 +514,8 @@ class SaleOrderImporter(Component):
     def _create(self, data):
         binding = super(SaleOrderImporter, self)._create(data)
         if binding.fiscal_position_id:
-            binding.odoo_id._compute_tax_id()
+            binding.odoo_id.order_line._compute_tax_id()
+            binding.odoo_id._recompute_taxes()
         return binding
 
     def _after_import(self, binding):
